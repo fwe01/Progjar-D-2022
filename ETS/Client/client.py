587a594c3b13bb108f550d17e229ad09625b07ea
Frederick William Edlim <frederickwilliamedlim@gmail.com>
2022-04-01 11:01:48 +0700

    style(code) : code cleanup

--- ETS/Client/client.py
+++ ETS/Client/client.py
@@ -26,8 +26,6 @@ def make_socket(destination_address='localhost', port=12000):
 
 def make_secure_socket(destination_address='localhost', port=10000):
     try:
-        # get it from https://curl.se/docs/caextract.html
-
         context = ssl.create_default_context(ssl.Purpose.CLIENT_AUTH)
         context.verify_mode = ssl.CERT_OPTIONAL
         context.load_verify_locations(os.getcwd() + '/domain.crt')
@@ -51,8 +49,6 @@ def deserialisasi(s):
 def send_command(command_str, is_secure=False):
     alamat_server = default_server_address[0]
     port_server = default_server_address[1]
-    #    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    # gunakan fungsi diatas
     if is_secure:
         sock = make_secure_socket(alamat_server, port_server)
     else:
@@ -62,24 +58,20 @@ def send_command(command_str, is_secure=False):
     try:
         logging.warning(f"sending message ")
         sock.sendall(command_str.encode())
-        # Look for the response, waiting until socket is done (no more data)
+
         data_received = ""  # empty string
         logging.warning("waiting server response")
+
         while True:
-            # socket does not receive all data at once, data comes in part, need to be concatenated at the end of process
             data = sock.recv(16)
             if data:
-                # data is not empty, concat with previous content
                 data_received += data.decode()
                 if "\r\n\r\n" in data_received:
                     break
             else:
-                # no more data, stop the process by break
                 break
-        # at this point, data_received (string) will contain all data coming from the socket
-        # to be able to use the data_received as a dict, need to load it using json.loads()
         hasil = deserialisasi(data_received)
-        logging.warning("data received from server:")
+        logging.warning(f"data received from server: {hasil}")
         return hasil
     except Exception as ee:
         logging.warning(f"error during data receiving {str(ee)}")
